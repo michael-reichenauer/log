2d62f0b49924a664932e5cab6932bd518d7a2a0b
Michael Reichenauer <michael.reichenauer@gmail.com>
2020-08-11 11:47:09 +0200

    Adjust

--- api/GetLog/index.js
+++ api/GetLog/index.js
@@ -4,13 +4,14 @@ module.exports = async function (context, req) {
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: JSON.stringify({ text: "Hello " + (req.query.name || req.body.name) })
+            body: JSON.stringify({ name: (req.query.name || req.body.name) })
         };
     }
     else {
         context.res = {
             status: 400,
-            body: JSON.stringify({ text: "Please pass a name on the query string or in the request body" })
+            text: "Missing name",
+            body: JSON.stringify({ name: "Please pass a name on the query string or in the request body" })
         };
     }
 };
