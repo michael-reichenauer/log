c35c740969b66799c3cc4f5cf6d3d6c37b3e236e
Michael Reichenauer <michael.reichenauer@gmail.com>
2020-08-18 08:18:23 +0200

    Adding ui version

---
+++ api/GetVersion/index.js
@@ -0,0 +1,12 @@
+//import preval from 'preval.macro'
+//const version = preval`module.exports = new Date().toLocaleString();`;
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: JSON.stringify({ version: "" })
+    };
+};
--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import TodoList from './TodoList';
+import preval from 'preval.macro'
+import useFetch from './utils/useFetch';
 
 export default function App() {
+
+
   const [count, setCount] = useState(0)
   const refresh = () => {
     setCount(c => c + 1)
@@ -9,9 +13,20 @@ export default function App() {
   const reload = () => {
     window.location.reload(true)
   }
+  const { response, loading, error } = useFetch(
+    "/api/GetVersion", null, count
+  );
+  if (error) {
+    return <div>Error: {"" + error}</div>
+  }
+  if (loading) {
+    return <div>Loading ...</div>
+  }
 
   return (
     <>
+      <p>Build Date: {preval`module.exports = new Date().toLocaleString();`}</p>
+      <p>Server Version: "{response && "" + response.version}"</p>
       <button onClick={reload}>Reload</button>
       <button onClick={refresh}>Refresh</button>
       <TodoList count={count} />
