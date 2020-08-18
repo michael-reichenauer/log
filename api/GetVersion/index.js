d899ea92ee814d88f1a3367fed140659b961cdd5
Michael Reichenauer <michael.reichenauer@gmail.com>
2020-08-18 11:05:23 +0200

    adjust

--- api/GetVersion/index.js
+++ api/GetVersion/index.js
@@ -4,9 +4,9 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-
+    const ver = process.env.REACT_APP_GIT_SHA
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: JSON.stringify({ version: "" })
+        body: JSON.stringify({ version: ver })
     };
 };
