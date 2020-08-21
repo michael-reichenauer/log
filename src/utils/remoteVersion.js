6c4c2a0b8612a5a9dcb805fe7b1ca7d8512ead14
Michael Reichenauer <michael.reichenauer@gmail.com>
2020-08-21 11:02:30 +0200

    fix remote build time

--- src/utils/remoteVersion.js
+++ src/utils/remoteVersion.js
@@ -33,3 +33,13 @@ export const updateUIIfRemoteVersionNewer = () => {
         });
 }
 
+// function dateToLocalISO(dateText) {
+//     const date = new Date(dateText)
+//     const off = date.getTimezoneOffset()
+//     const absoff = Math.abs(off)
+//     return (new Date(date.getTime() - off * 60 * 1000).toISOString().substr(0, 23) +
+//         (off > 0 ? '-' : '+') +
+//         (absoff / 60).toFixed(0).padStart(2, '0') + ':' +
+//         (absoff % 60).toString().padStart(2, '0'))
+// }
+
